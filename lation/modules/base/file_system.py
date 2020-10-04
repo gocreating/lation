e49f223ba5ad80ef9da98f946b4d4da3f6a25efd
gocreating <gocreating@gmail.com>
2020-10-04 15:59:37 +0800

    Refine file system

--- lation/modules/base/file_system.py
+++ lation/modules/base/file_system.py
@@ -7,17 +7,20 @@ class FileSystem(LocalStorage):
     def __init__(self, **kwargs):
         self.cwd = os.getcwd()
 
+    def _get_path(self, name):
+        return os.path.join(self.cwd, *name)
+
     def get_mime_type(self, name, **kwargs):
-        target_path = os.path.join(self.cwd, *name)
+        target_path = self._get_path(name)
         mime_type = mimetypes.guess_type(target_path)[0]
         return mime_type
 
     def is_file(self, name, **kwargs):
-        target_path = os.path.join(self.cwd, *name)
+        target_path = self._get_path(name)
         return os.path.isfile(target_path)
 
     def is_directory(self, name, **kwargs):
-        target_path = os.path.join(self.cwd, *name)
+        target_path = self._get_path(name)
         return os.path.isdir(target_path)
 
     def change_directory(self, serialized_name):
@@ -25,11 +28,11 @@ class FileSystem(LocalStorage):
         self.cwd = cwd
 
     def list_directory(self, name=None, **kwargs):
-        target_path = os.path.join(self.cwd, *name)
+        target_path = self._get_path(name)
         return os.listdir(target_path)
 
     def create_directory(self, name, **kwargs):
-        target_path = os.path.join(self.cwd, *name)
+        target_path = self._get_path(name)
         if not os.path.exists(target_path):
             os.makedirs(target_path)
         return target_path
