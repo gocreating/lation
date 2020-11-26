e6271c8f4c01e4afde9ef08222427c0f98e5f8cb
gocreating <gocreating@gmail.com>
2020-11-27 02:42:42 +0800

    Add version endpoint

--- lation/modules/base_fastapi/base_fastapi.py
+++ lation/modules/base_fastapi/base_fastapi.py
@@ -1,8 +1,14 @@
+import os
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 class Liveness(BaseModel):
     status: int
+
+class Version(BaseModel):
+    status: int
+    data: str
 
 class BaseFastAPI(FastAPI):
     def __init__(self):
@@ -11,3 +17,9 @@ class BaseFastAPI(FastAPI):
         @self.get('/', response_model=Liveness)
         def liveness():
             return {'status': 0}
+
+        @self.get('/version', response_model=Version)
+        def version():
+            IMAGE_TAG = os.getenv('IMAGE_TAG')
+            version = IMAGE_TAG if IMAGE_TAG else 'local'
+            return {'status': 0, 'data': version}
