bf2fa62518c1c69d62d24cd1f677a1daa0e0b727
gocreating <gocreating@gmail.com>
2021-01-08 02:05:31 +0800

    Update timestamp default

--- lation/core/orm.py
+++ lation/core/orm.py
@@ -1,5 +1,8 @@
+import datetime
+
 from sqlalchemy import Column, Integer, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
 
 from lation.core.env import get_env
 
@@ -10,7 +13,7 @@ class BaseClass(object):
     __table_args__ = {'schema': APP}
 
     id =  Column(Integer, primary_key=True)
-    createdAt = Column(DateTime)
-    updatedAt = Column(DateTime)
+    createdAt = Column(DateTime, server_default=func.now())
+    updatedAt = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
 Base = declarative_base(cls=BaseClass)
