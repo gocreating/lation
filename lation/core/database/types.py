568907463e00ab54a3e7138f711f3207ebbf3623
gocreating <gocreating@gmail.com>
2021-01-21 23:27:52 +0800

    Organize column types

---
+++ lation/core/database/types.py
@@ -0,0 +1,10 @@
+from sqlalchemy import JSON, Integer, String
+
+STRING_XS_SIZE = 25
+STRING_S_SIZE = 100
+STRING_M_SIZE = 250
+STRING_L_SIZE = 4000
+
+JSON = JSON
+Integer = Integer
+String = String
