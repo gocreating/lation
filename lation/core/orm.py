b1a2d177be8bb215b6125564c56bcb9fa913ef9f
gocreating <gocreating@gmail.com>
2021-01-31 02:54:55 +0800

    Fix timestamp column naming

--- lation/core/orm.py
+++ lation/core/orm.py
@@ -67,7 +67,7 @@ class BaseClass:
                 return None
         return Column(Integer, primary_key=True)
 
-    createdAt = Column(DateTime, index=True, server_default=func.now())
-    updatedAt = Column(DateTime, index=True, server_default=func.now(), onupdate=func.now())
+    created_at = Column(DateTime, index=True, server_default=func.now())
+    updated_at = Column(DateTime, index=True, server_default=func.now(), onupdate=func.now())
 
 Base = declarative_base(cls=BaseClass, metadata=Database.get_metadata())
