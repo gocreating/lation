2fb098999a975bd9249013acd3d72d2b4b5bcc34
gocreating <gocreating@gmail.com>
2021-01-31 19:08:04 +0800

    Add line message experiment

--- lation/modules/stock/routers/experiment.py
+++ lation/modules/stock/routers/experiment.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from fastapi import APIRouter
 
 from lation.modules.base.models.notification import Notification
+from lation.modules.stock.line_api_client import LineAPIClient
 
 
 router = APIRouter()
@@ -11,4 +12,18 @@ router = APIRouter()
 async def push_smtp_notification():
     notification = Notification()
     notification.send_email((Path(__file__).parent / '../data/email_template.html').resolve())
+    return None
+
+@router.get('/send-line-notification', tags=['experiment'])
+async def push_line_notification():
+    line_api_client = LineAPIClient()
+    res_data = line_api_client.push_message('U5abfe9090acd8357516e26604a3606b6', [{
+        'type': 'image',
+        'originalContentUrl': 'https://stock-api.lation.app:5555/static/latest-push-content-cut-words.png',
+        'previewImageUrl': 'https://stock.lation.app/logo.png',
+    }])
+    res_data = line_api_client.push_message('U5abfe9090acd8357516e26604a3606b6', [{
+        'type': 'text',
+        'text': '嗨，Lation Stock 使用者您好',
+    }])
     return None
