752d8561d012c5e6aa5ca7ae5df6920812d3a060
Chih-Ping Weng <chihping.weng@onedegree.hk>
2021-02-12 02:35:11 +0800

    Add get_friendship_status to line_api_client

--- lation/modules/stock/line_api_client.py
+++ lation/modules/stock/line_api_client.py
@@ -6,13 +6,19 @@ LINE_CHANNEL_ACCESS_TOKEN = get_env('LINE_CHANNEL_ACCESS_TOKEN')
 
 class LineAPIClient(HttpClient):
 
-    def __init__(self, channel_access_token=None):
+    def __init__(self, access_token=None):
         super().__init__(host='https://api.line.me')
-        if not channel_access_token:
-            channel_access_token = LINE_CHANNEL_ACCESS_TOKEN
-        self.channel_access_token = channel_access_token
+        if not access_token:
+            access_token = LINE_CHANNEL_ACCESS_TOKEN
+        self.access_token = access_token
+
+    # https://developers.line.biz/zh-hant/docs/line-login/link-a-bot/#use-social-api
+    def get_friendship_status(self) -> dict:
+        return self.get_json('/friendship/v1/status',
+                              headers={'Authorization': f'Bearer {self.access_token}'})
+
 
     def push_message(self, to:str, messages) -> dict:
         return self.post_json('/v2/bot/message/push',
-                              headers={'Authorization': f'Bearer {self.channel_access_token}'},
+                              headers={'Authorization': f'Bearer {self.access_token}'},
                               json={'to': to, 'messages': messages})
