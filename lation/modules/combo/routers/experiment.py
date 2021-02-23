e2693d0fa6bca9964b39ce74d736e555c9014ac0
gocreating <gocreating@gmail.com>
2021-02-24 01:37:11 +0800

    Update

--- lation/modules/combo/routers/experiment.py
+++ lation/modules/combo/routers/experiment.py
@@ -10,12 +10,12 @@ router = APIRouter()
 
 class CurrencyClient(HttpClient):
 
-    def get_top_200_symbols(self):
+    def get_top_n_symbols(self, n):
         res = self.get('https://coinmarketcap.com/currencies/volume/monthly/')
         soup = BeautifulSoup(res.text, 'html.parser')
         cells = soup.find_all('td', class_='cmc-table__cell--sort-by__symbol')
         symbols = [cell.getText() for cell in cells]
-        return symbols
+        return symbols[:n]
 
     # https://www.binance.com/en/markets
     def get_binance_symbols(self):
@@ -23,13 +23,33 @@ class CurrencyClient(HttpClient):
         symbols = [datum['name'] for datum in data['data']]
         return symbols
 
+    # https://www.huobi.com/zh-cn/assetintro/
+    def get_huobi_symbols(self):
+        data = self.get_json('https://www.huobi.com/-/x/pro/v2/beta/common/currencies')
+        symbols = [datum['display_name'] for datum in data['data']]
+        return symbols
+
+    # https://www.coinbase.com/price/s/listed?resolution=hour&page=2
+    def get_coinbase_symbols(self):
+        symbols = []
+        for page in range(1,6):
+            data = self.get_json(f'https://www.coinbase.com/api/v2/assets?filter=all&limit=30&page={page}')
+            symbols += [datum['symbol'] for datum in data['data']]
+        return symbols
+
 @router.get('/reveal-currency', tags=['experiment'])
-async def reveal_currency():
+async def reveal_currency(top_n:int=50):
     client = CurrencyClient()
-    top_200_symbols = client.get_top_200_symbols()
+    top_n_symbols = client.get_top_n_symbols(top_n)
     binance_symbols = client.get_binance_symbols()
+    huobi_symbols = client.get_huobi_symbols()
+    coinbase_symbols = client.get_coinbase_symbols()
     return {
-        'top_200_symbols': top_200_symbols,
+        'top_n_symbols': top_n_symbols,
         'binance_symbols': binance_symbols,
-        'top_symbols_not_in_binance': [top_symbol for top_symbol in top_symbols if top_symbol not in binance_symbols],
+        'huobi_symbols': huobi_symbols,
+        'coinbase_symbols': coinbase_symbols,
+        'top_symbols_not_in_binance': [top_symbol for top_symbol in top_n_symbols if top_symbol not in binance_symbols],
+        'top_symbols_not_in_huobi': [top_symbol for top_symbol in top_n_symbols if top_symbol not in huobi_symbols],
+        'top_symbols_not_in_coinbase': [top_symbol for top_symbol in top_n_symbols if top_symbol not in coinbase_symbols],
     }
