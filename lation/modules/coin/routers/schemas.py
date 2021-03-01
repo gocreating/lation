55cf9c9eb3c61ea5d50f2c406f0975dca9b11165
gocreating <gocreating@gmail.com>
2021-03-02 02:15:45 +0800

    Update

--- lation/modules/coin/routers/schemas.py
+++ lation/modules/coin/routers/schemas.py
@@ -1,12 +1,39 @@
-from typing import Optional
+import enum
+from typing import Dict, List, Literal, Optional
 
 from pydantic import BaseModel
 
 
+class BitfinexFundingSymbol(str, enum.Enum):
+    USD = 'fUSD'
+
+class BitfinexSymbolFundingAmountStrategy(BaseModel):
+    hold_amount: float = 0
+    min_per_offer_amount: float = 50
+    max_per_offer_amount: float = 500
+
+class BitfinexSymbolFundingRateStrategy(BaseModel):
+    min_per_offer_rate: float = 0
+
+class BitfinexSymbolFundingRateToPeriodRule(BaseModel):
+    gte_rate: float
+    lt_rate: float
+    period: int
+
+class BitfinexSymbolFundingStrategy(BaseModel):
+    enabled: bool = False
+    amount_strategy: BitfinexSymbolFundingAmountStrategy
+    rate_strategy: BitfinexSymbolFundingRateStrategy
+    rate_to_period_rules: List[BitfinexSymbolFundingRateToPeriodRule]
+
+class BitfinexFundingStrategy(BaseModel):
+    enabled: bool = False
+    symbol_strategy: Dict[str, BitfinexSymbolFundingStrategy]
+
 class EndUserBitfinexConfigSchema(BaseModel):
     api_key: Optional[str]
     api_secret: Optional[str]
-    config: Optional[dict]
+    funding_strategy: Optional[BitfinexFundingStrategy]
 
     class Config:
         orm_mode = True
--- lation/modules/coin/routers/user.py
+++ lation/modules/coin/routers/user.py
@@ -7,7 +7,7 @@ from lation.modules.base_fastapi.decorators import managed_transaction
 from lation.modules.base_fastapi.dependencies import get_session
 from lation.modules.base_fastapi.routers.schemas import ResponseSchema as Response, StatusEnum
 from lation.modules.coin.models.config import EndUserBitfinexConfig
-from lation.modules.coin.routers.schemas import EndUserBitfinexConfigSchema
+from lation.modules.coin.routers.schemas import BitfinexFundingStrategy, BitfinexFundingSymbol, BitfinexSymbolFundingAmountStrategy, BitfinexSymbolFundingRateStrategy, BitfinexSymbolFundingRateToPeriodRule, BitfinexSymbolFundingStrategy, EndUserBitfinexConfigSchema
 from lation.modules.customer.dependencies import login_required, get_current_user
 
 
@@ -17,16 +17,34 @@ router = APIRouter()
             tags=['end_user'],
             dependencies=[Depends(login_required)],
             response_model=Response[EndUserBitfinexConfigSchema])
-def get_bitfinex_config(end_user=Depends(get_current_user), session:Session=Depends(get_session)):
-    return Response[EndUserBitfinexConfigSchema](status=StatusEnum.SUCCESS, data=end_user.end_user_bitfinex_config)
+async def get_bitfinex_config(end_user=Depends(get_current_user), session:Session=Depends(get_session)):
+    end_user_bitfinex_config = end_user.end_user_bitfinex_config
+    if not end_user_bitfinex_config:
+        end_user_bitfinex_config = EndUserBitfinexConfigSchema(
+            funding_strategy=BitfinexFundingStrategy(
+                symbol_strategy={
+                    BitfinexFundingSymbol.USD: BitfinexSymbolFundingStrategy(
+                        amount_strategy=BitfinexSymbolFundingAmountStrategy(),
+                        rate_strategy=BitfinexSymbolFundingRateStrategy(),
+                        rate_to_period_rules=[
+                            BitfinexSymbolFundingRateToPeriodRule(gte_rate=0, lt_rate=10, period=2),
+                            BitfinexSymbolFundingRateToPeriodRule(gte_rate=10, lt_rate=15, period=14),
+                            BitfinexSymbolFundingRateToPeriodRule(gte_rate=15, lt_rate=20, period=30),
+                            BitfinexSymbolFundingRateToPeriodRule(gte_rate=20, lt_rate=2555, period=120),
+                        ]
+                    )
+                }
+            )
+        )
+    return Response[EndUserBitfinexConfigSchema](status=StatusEnum.SUCCESS, data=end_user_bitfinex_config)
 
 @router.patch('/configs/bitfinex',
              tags=['end_user'],
              dependencies=[Depends(login_required)],
              response_model=Response[EndUserBitfinexConfigSchema])
 @managed_transaction
-def update_bitfinex_config(config:EndUserBitfinexConfigSchema,
-                           end_user=Depends(get_current_user), session:Session=Depends(get_session)):
+async def update_bitfinex_config(config:EndUserBitfinexConfigSchema,
+                                 end_user=Depends(get_current_user), session:Session=Depends(get_session)):
     end_user_bitfinex_config = end_user.end_user_bitfinex_config
     if not end_user_bitfinex_config:
         end_user_bitfinex_config = EndUserBitfinexConfig(end_user=end_user)
@@ -35,6 +53,6 @@ def update_bitfinex_config(config:EndUserBitfinexConfigSchema,
         end_user_bitfinex_config.api_key = config.api_key
     if config.api_secret:
         end_user_bitfinex_config.api_secret = config.api_secret
-    if config.config:
-        end_user_bitfinex_config.config = config.config
+    if config.funding_strategy:
+        end_user_bitfinex_config.funding_strategy = config.funding_strategy.dict()
     return Response[EndUserBitfinexConfigSchema](status=StatusEnum.SUCCESS, data=end_user_bitfinex_config)
