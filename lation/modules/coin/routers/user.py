530d31252b3d93f5406d8d5f89d485cdb0454570
gocreating <gocreating@gmail.com>
2021-03-06 20:00:56 +0800

    Add permission checking

--- lation/modules/coin/routers/user.py
+++ lation/modules/coin/routers/user.py
@@ -1,13 +1,13 @@
 from typing import Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from lation.modules.base_fastapi.decorators import managed_transaction
 from lation.modules.base_fastapi.dependencies import get_session
 from lation.modules.base_fastapi.routers.schemas import ResponseSchema as Response, StatusEnum
 from lation.modules.coin.models.config import EndUserBitfinexConfig
-from lation.modules.coin.bitfinex_api_client import get_default_bitfinex_funding_strategy
+from lation.modules.coin.bitfinex_api_client import BitfinexAPIClient, get_default_bitfinex_funding_strategy
 from lation.modules.coin.routers.schemas import EndUserBitfinexConfigSchema
 from lation.modules.customer.dependencies import login_required, get_current_user
 
@@ -41,6 +41,18 @@ async def update_bitfinex_config(config:EndUserBitfinexConfigSchema,
         end_user_bitfinex_config.api_key = config.api_key
     if config.api_secret:
         end_user_bitfinex_config.api_secret = config.api_secret
+    if config.api_key and config.api_secret:
+        bitfinex_api_client = BitfinexAPIClient(config.api_key, config.api_secret)
+        permissions = bitfinex_api_client.get_user_permissions()
+        permissionMap = {
+            permission.scope: {
+                'read': permission.read,
+                'write': permission.write,
+            }
+            for permission in permissions
+        }
+        if not (permissionMap['funding']['read'] == 1 and permissionMap['funding']['write'] == 1  and permissionMap['wallets']['read'] == 1):
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Your Bitfinex API key permission is wrong')
     if config.funding_strategy:
         end_user_bitfinex_config.funding_strategy = config.funding_strategy.dict()
     return Response[EndUserBitfinexConfigSchema](status=StatusEnum.SUCCESS, data=end_user_bitfinex_config)
