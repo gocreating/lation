0ee3731009a8455e1ec4fede6da94884e49686c2
gocreating <gocreating@gmail.com>
2021-03-09 23:13:08 +0800

    Start to implement funding strategy

--- lation/modules/coin/models/end_user.py
+++ lation/modules/coin/models/end_user.py
@@ -1,7 +1,19 @@
 from lation.modules.base.models.end_user import EndUser
+from lation.modules.coin.bitfinex_api_client import BitfinexAPIClient, get_default_bitfinex_funding_strategy
 
 
 def apply_bitfinex_funding_strategy(self, ask_rate:float):
-    print(ask_rate)
+    config = self.end_user_bitfinex_config
+    if not config:
+        return
+    api_key = config.api_key
+    api_secret = config.api_secret
+    strategy = config.funding_strategy
+    if not api_key or not api_secret or not strategy:
+        return
+    api_client = BitfinexAPIClient(api_key=api_key, api_secret=api_secret)
+    wallets = api_client.get_user_wallets()
+    funding_wallet = next(wallet for wallet in wallets if wallet.wallet_type == 'funding')
+    print(ask_rate, funding_wallet.available_balance)
 
 EndUser.apply_bitfinex_funding_strategy = apply_bitfinex_funding_strategy
