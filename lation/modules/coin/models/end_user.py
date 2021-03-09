5931a7ac6c4c4201995cf101547f299abf4f9431
gocreating <gocreating@gmail.com>
2021-03-10 01:25:25 +0800

    Implement funding strategy

--- lation/modules/coin/models/end_user.py
+++ lation/modules/coin/models/end_user.py
@@ -1,5 +1,8 @@
+import time
+
 from lation.modules.base.models.end_user import EndUser
 from lation.modules.coin.bitfinex_api_client import BitfinexAPIClient, get_default_bitfinex_funding_strategy
+from lation.modules.coin.routers.schemas import BitfinexFundingStrategy
 
 
 def apply_bitfinex_funding_strategy(self, ask_rate:float):
@@ -8,12 +11,49 @@ def apply_bitfinex_funding_strategy(self, ask_rate:float):
         return
     api_key = config.api_key
     api_secret = config.api_secret
-    strategy = config.funding_strategy
+    strategy = BitfinexFundingStrategy(**config.funding_strategy)
     if not api_key or not api_secret or not strategy:
         return
+    if not strategy.enabled:
+        return
+
     api_client = BitfinexAPIClient(api_key=api_key, api_secret=api_secret)
     wallets = api_client.get_user_wallets()
-    funding_wallet = next(wallet for wallet in wallets if wallet.wallet_type == 'funding')
-    print(ask_rate, funding_wallet.available_balance)
+    for currency in ['USD']:
+        symbol = f'f{currency}'
+        symbol_strategy = strategy.symbol_strategy.get(symbol, None)
+        if not symbol_strategy:
+            continue
+        if not symbol_strategy.enabled:
+            continue
+
+        # cancel offers
+        funding_offers = api_client.get_user_funding_offers(symbol)
+        for offer in funding_offers:
+            cancel_funding_offer = api_client.cancel_user_funding_offer(offer.id)
+            time.sleep(0.2)
+        time.sleep(1)
+
+        # submit offers
+        amount_strategy = symbol_strategy.amount_strategy
+        rate_strategy = symbol_strategy.rate_strategy
+        rate_to_period_rules = symbol_strategy.rate_to_period_rules
+
+        funding_wallet = next(wallet for wallet in wallets
+                              if wallet.wallet_type == 'funding' and wallet.currency == currency)
+        if not funding_wallet:
+            continue
+        available_balance = funding_wallet.available_balance - amount_strategy.hold_amount
+        while available_balance >= amount_strategy.min_per_offer_amount:
+            offer_amount = min(available_balance, amount_strategy.max_per_offer_amount)
+            available_balance -= offer_amount
+            offer_rate = max(ask_rate, rate_strategy.min_per_offer_rate / 36500)
+            offer_period = 2
+            for rule in rate_to_period_rules:
+                if rule.gte_rate / 36500 <= offer_rate <= rule.lt_rate / 36500:
+                    offer_period = rule.period
+                    break
+            submit_funding_offer = api_client.submit_user_funding_offer(BitfinexAPIClient.FundingOfferTypeEnum.LIMIT, symbol, f'{offer_amount}', f'{offer_rate}', offer_period)
+            time.sleep(0.2)
 
 EndUser.apply_bitfinex_funding_strategy = apply_bitfinex_funding_strategy
