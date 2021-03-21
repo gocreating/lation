1a12af87d86124803d0e1a83f86ea99479caa1c1
gocreating <gocreating@gmail.com>
2021-03-21 23:15:23 +0800

    Add payment and payment_item model

--- lation/modules/base/models/payment.py
+++ lation/modules/base/models/payment.py
@@ -1,8 +1,10 @@
 from datetime import datetime
 
-from sqlalchemy import Column
+from sqlalchemy import Column, ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import backref, relationship
 
-from lation.core.database.types import STRING_M_SIZE, STRING_XS_SIZE, String
+from lation.core.database.types import STRING_M_SIZE, STRING_S_SIZE, STRING_XS_SIZE, Float, Integer, String
 from lation.core.env import get_env
 from lation.core.orm import Base, SingleTableInheritanceMixin
 from lation.modules.base.vendors.ecpay_payment_sdk import ECPayPaymentSdk
@@ -14,6 +16,24 @@ PAYMENT_GATEWAY_ECPAY_MERCHANT_ID = get_env('PAYMENT_GATEWAY_ECPAY_MERCHANT_ID')
 PAYMENT_GATEWAY_ECPAY_HASH_KEY = get_env('PAYMENT_GATEWAY_ECPAY_HASH_KEY')
 PAYMENT_GATEWAY_ECPAY_HASH_IV = get_env('PAYMENT_GATEWAY_ECPAY_HASH_IV')
 PAYMENT_REDIRECT_URL=f'{FRONTEND_HOST}/payment/result'
+
+
+class Payment(Base):
+    __tablename__ = 'payment'
+
+    @hybrid_property
+    def total_billed_amount(self) -> float:
+        return sum([payment_item.billed_amount for payment_item in self.payment_items])
+
+
+class PaymentItem(Base):
+    __tablename__ = 'payment_item'
+
+    payment_id = Column(Integer, ForeignKey('payment.id'), index=True)
+    payment = relationship('Payment', foreign_keys=[payment_id], backref=backref('payment_items'))
+
+    item_name = Column(String(STRING_S_SIZE))
+    billed_amount = Column(Float)
 
 
 class PaymentGateway(Base, SingleTableInheritanceMixin):
