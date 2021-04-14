f7c00fd71f0bf526ff3ee5886a06b403a09c9668
gocreating <gocreating@gmail.com>
2021-04-15 00:23:34 +0800

    Add subaccount enum

--- lation/modules/coin/dependencies.py
+++ lation/modules/coin/dependencies.py
@@ -1,3 +1,4 @@
+import enum
 from typing import Optional
 
 from fastapi import Depends, HTTPException, status
@@ -11,6 +12,9 @@ from lation.modules.customer.dependencies import login_required, get_current_use
 FTX_API_KEY = get_env('FTX_API_KEY')
 FTX_API_SECRET = get_env('FTX_API_SECRET')
 
+class SubaccountNameEnum(str, enum.Enum):
+    期现套利子帳戶 = '期现套利子帳戶'
+
 async def get_bitfinex_api_client(end_user=Depends(get_current_user)) -> BitfinexAPIClient:
     end_user_bitfinex_config = end_user.end_user_bitfinex_config
     if not end_user_bitfinex_config:
@@ -21,8 +25,8 @@ async def get_bitfinex_api_client(end_user=Depends(get_current_user)) -> Bitfine
                                             api_secret=end_user_bitfinex_config.api_secret)
     return bitfinex_api_client
 
-async def get_current_ftx_rest_api_client(subaccount_name: Optional[str] = None) -> FTXRestAPIClient:
-    # TODO: read api_key, api_secret, and subaccount_name (期现套利子帳戶) from current_user's config
+async def get_current_ftx_rest_api_client(subaccount_name: Optional[SubaccountNameEnum] = None) -> FTXRestAPIClient:
+    # TODO: read api_key, api_secret, and subaccount_name from current_user's config
     ftx_rest_api_client = FTXRestAPIClient(api_key=FTX_API_KEY,
                                            api_secret=FTX_API_SECRET,
                                            subaccount_name=subaccount_name)
