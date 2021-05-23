ad3c9437d20465e9113b93e2e64ca3ce210f664f
gocreating <gocreating@gmail.com>
2021-05-23 14:31:21 +0800

    Update strategy config

--- lation/modules/spot_perp_bot/schemas.py
+++ lation/modules/spot_perp_bot/schemas.py
@@ -19,32 +19,32 @@ class FtxArbitrageStrategyConfig(BaseModel):
     class IncreasePairConfig(BaseModel):
         enabled: bool = True
         lt_leverage: float = 11.0
-        gt_spread_rate: float = 0.0035
+        gt_spread_rate: float = 0.003,
         max_balance_rate: float = 0.2
         leverage_diff_to_quote_amount_rules: List[FtxArbitrageStrategyConfig.LeverageDiffToQuoteAmountRule] = []
 
     class AlwaysIncreasePairConfig(BaseModel):
         enabled: bool = True
         gt_spread_rate: float = 0.0065
-        quote_amount: Decimal = Decimal('50')
+        quote_amount: Decimal = Decimal('30')
 
     class DecreasePairConfig(BaseModel):
         enabled: bool = True
         gt_leverage: float = 17.0
-        lt_spread_rate: float = 0
+        lt_spread_rate: float = -0.001
         leverage_diff_to_quote_amount_rules: List[FtxArbitrageStrategyConfig.LeverageDiffToQuoteAmountRule] = []
 
     class AlwaysDecreasePairConfig(BaseModel):
         enabled: bool = True
         lt_spread_rate: float = -0.001
-        quote_amount: Decimal = Decimal('50')
+        quote_amount: Decimal = Decimal('30')
 
     class ClosePairConfig(BaseModel):
         gt_leverage: float = 20.0
 
     class GarbageCollectConfig(BaseModel):
         enabled: bool = False
-        lt_spread_rate: float = 0.0005
+        lt_spread_rate: float = 0.001
 
     alarm: Optional[FtxArbitrageStrategyConfig.AlarmConfig]
     increase_pair: Optional[FtxArbitrageStrategyConfig.IncreasePairConfig]
